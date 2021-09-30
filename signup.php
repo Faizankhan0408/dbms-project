0b9e1655852d2e74098432585abb3be2640da902
Rajat376 <89566014+Rajat376@users.noreply.github.com>
2021-09-30 16:28:58 +0530

    Update signup.php

--- signup.php
+++ signup.php
@@ -9,22 +9,46 @@ die('Failed to connect to server');
 } 
 $query="INSERT INTO users (your_name,username,email,password) Values ('$your_name','$username','$email','$password')";
 $results = mysqli_query($link, $query); 
+if($your_name)
+{if($password)
+	{
 if($results == FALSE) 
 {
 echo '<script type="text/javascript">
 
-          window.onload = function () { alert("Usename taken or email already registered"); }
+          window.onload = function () { alert("Usename taken or email already registered or all field not filled"); }
 
 </script>';
 include 'main.html';
 }
 else {
-'<script type="text/javascript">
+	echo'<script type="text/javascript">
 
           window.onload = function () { alert("Thank you for signing up"); }
 
 </script>';
 include 'main.html';
 } 
+	}
+
+else{
+	echo'<script type="text/javascript">
+
+          window.onload = function () { alert("please enter password"); }
+
+</script>';
+	include 'main.html';
+}
+}
+
+
+else
+{echo'<script type="text/javascript">
+
+          window.onload = function () { alert("please fill your name"); }
+
+</script>';
+	include 'main.html';
+}
 
 ?> 
