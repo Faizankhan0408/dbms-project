7491e63d4293fc91a69fe949021c20d53c558d4a
Rajat376 <89566014+Rajat376@users.noreply.github.com>
2021-10-04 17:51:50 +0530

    Update login.php

--- login.php
+++ login.php
@@ -5,66 +5,46 @@ $password=$_POST['password'];
 if(!$link){ 
 die('Failed to connect to server'); 
 } 
-$query="select email,password from users where email='$email' and password='$password'";
-$results = mysqli_query($link, $query); 
-$log=mysqli_fetch_array($results);
-if($email== $log['email'] && $password == $log['password']){ 
-	//$count = mysql_num_rows($result);
-	$count = 1; 
-	} 
-	else{ 
-	//Login failed 
-	include('main.html'); 
-	echo '<script type="text/javascript">
-	window.onload = function () { alert("Incorrect password!!"); 
-</script>'; 
-	exit(); 
-	} 
-	if( $count == 1){ 
-		//Login successful set session variables and redirect to main page.
-		echo'<script type="text/javascript">
-           window.onload = function () { alert("Thank you for login up"); 
- </script>';
-include 'main.html'; 
-		session_start(); 
-		$_SESSION['IS_AUTHENTICATED'] = 1; 
-		$_SESSION['USER_ID'] = $login; 
-		header('location:main.html'); 
-		} 
-		else{ 
-		//Login failed 
-		include('main.html'); 
-		echo '<script type="text/javascript">
-           window.onload = function () { alert("Incorrect password!!"); 
- </script>';
-include 'main.html'; 
-		exit(); 
-		} 
-		
-		
-// if($email)
-// {
-//     if($password)
-// 	{
-// if($results == FALSE) 
-// {
-// echo '<script type="text/javascript">
-//           window.onload = function () { alert("Incorrect password!!"); }
-// </script>';
-// include 'main.html';
-// }
-// else {
-// 	echo'<script type="text/javascript">
-//           window.onload = function () { alert("Thank you for login up"); }
-// </script>';
-// include 'main.html';
-// } 
-// 	}
+$query="select email,password from users;";
+$results = mysqli_query($link,$query); 
+$log=mysqli_fetch_assoc($results);
 
-// else{
-// 	echo'<script type="text/javascript">
-//           window.onload = function () { alert("please enter password"); }
-// </script>';
-// 	include 'main.html';
-// }
-// }
+if($email)
+{if($password)
+	{while ($row = mysqli_fetch_assoc($results))
+{
+if($row['email']==$email && $row['password']==$password)
+{echo'<script type="text/javascript">
+           window.onload = function () { alert("You are logged in"); }
+ </script>';
+ session_start(); 
+		$_SESSION['IS_AUTHENTICATED'] = 1; 
+		$_SESSION['USER_EMAIL'] = $email;
+		include "main.html";
+ goto chance;
+			
+		}
+	
+}
+echo'<script type="text/javascript">
+           window.onload = function () { alert("incorrect credentials"); }
+ </script>';
+	include "main.html";
+	chance:
+	
+}
+else
+	{echo'<script type="text/javascript">
+           window.onload = function () { alert("Please enter password"); }
+ </script>';
+	include "main.html";
+}
+}
+else
+{echo'<script type="text/javascript">
+           window.onload = function () { alert("Please enter email");} 
+ </script>';
+ 
+	include "main.html";
+}
+
