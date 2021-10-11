7dbfc0b1422e774e17bfd692adbf00d912a447d1
Mohammad Faizan <86049664+Faizankhan0408@users.noreply.github.com>
2021-10-11 10:52:14 +0530

    updating login alert

--- login.php
+++ login.php
@@ -1,50 +1,36 @@
 <?php
-$link = mysqli_connect('localhost', 'root', '', 'jk');
-$email=$_POST['email'];
-$password=$_POST['password'];
-if(!$link){ 
-die('Failed to connect to server'); 
-} 
-$query="select email,password from users;";
-$results = mysqli_query($link,$query); 
-$log=mysqli_fetch_assoc($results);
-
-if($email)
-{if($password)
-	{while ($row = mysqli_fetch_assoc($results))
-{
-if($row['email']==$email && $row['password']==$password)
-{echo'<script type="text/javascript">
-           window.onload = function () { alert("You are logged in"); }
- </script>';
- session_start(); 
-		$_SESSION['IS_AUTHENTICATED'] = 1; 
-		$_SESSION['USER_EMAIL'] = $email;
-		include "main.html";
- goto chance;
-			
+session_start();
+?>
+		<?php
+		$link = mysqli_connect('localhost', 'root', '', 'jk');
+		$email=$_POST['email'];
+		$password=$_POST['password'];
+		if(!$link){ 
+		die('Failed to connect to server'); 
+		} 
+		$query="select email,password from users;";
+		$results = mysqli_query($link,$query); 
+		$log=mysqli_fetch_assoc($results);
+		
+		
+		while ($row = mysqli_fetch_assoc($results))
+		{
+		if($row['email']==$email && $row['password']==$password){
+		// {echo'<script type="text/javascript">
+		// 		   window.onload = function () { alert("You are logged in"); }
+		//  </script>';
+		$_SESSION['status']="You are Logged !!";
+        $_SESSION['status_code']="success";
+        include 'main.html';
 		}
-	
-}
-echo'<script type="text/javascript">
-           window.onload = function () { alert("incorrect credentials"); }
- </script>';
-	include "main.html";
-	chance:
-	
-}
-else
-	{echo'<script type="text/javascript">
-           window.onload = function () { alert("Please enter password"); }
- </script>';
-	include "main.html";
-}
-}
-else
-{echo'<script type="text/javascript">
-           window.onload = function () { alert("Please enter email");} 
- </script>';
- 
-	include "main.html";
-}
-
+		else{
+			// echo'';
+			$_SESSION['status']="Invalid Credentials";
+            $_SESSION['status_code']="error";
+		}
+	}
+		?>
+		<?php
+		include 'script_log.php';
+		?>
+		
