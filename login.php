463bafa7219e1cbf034c65625253cb177e98d45c
Rajat376 <89566014+Rajat376@users.noreply.github.com>
2021-10-11 11:36:53 +0530

    Update login.php

--- login.php
+++ login.php
@@ -1,36 +1,59 @@
 <?php
 session_start();
 ?>
-		<?php
-		$link = mysqli_connect('localhost', 'root', '', 'jk');
-		$email=$_POST['email'];
-		$password=$_POST['password'];
-		if(!$link){ 
-		die('Failed to connect to server'); 
-		} 
-		$query="select email,password from users;";
-		$results = mysqli_query($link,$query); 
-		$log=mysqli_fetch_assoc($results);
-		
-		
-		while ($row = mysqli_fetch_assoc($results))
-		{
-		if($row['email']==$email && $row['password']==$password){
-		// {echo'<script type="text/javascript">
-		// 		   window.onload = function () { alert("You are logged in"); }
-		//  </script>';
-		$_SESSION['status']="You are Logged !!";
-        $_SESSION['status_code']="success";
-        include 'main.html';
+<?php
+
+$link = mysqli_connect('localhost', 'root', '', 'jk');
+$email=$_POST['email'];
+$password=$_POST['password'];
+if(!$link){ 
+die('Failed to connect to server'); 
+} 
+$query="select email,password,your_name from users;";
+$results = mysqli_query($link,$query); 
+$log=mysqli_fetch_assoc($results);
+
+if($email)
+{if($password)
+	{while ($row = mysqli_fetch_assoc($results))
+{
+if($row['email']==$email && $row['password']==$password)
+{$_SESSION['status']="You are logged in";
+            $_SESSION['status_code']="success";
+		 
+		$_SESSION['IS_AUTHENTICATED'] = 1; 
+		$_SESSION['USER_EMAIL'] = $email;
+		$_SESSION['USER_NAME'] = $row['your_name'];
+		include "main.html";
+ goto chance;
+			
 		}
-		else{
-			// echo'';
-			$_SESSION['status']="Invalid Credentials";
+	
+}
+$_SESSION['status']="Invalid Credentials";
             $_SESSION['status_code']="error";
-		}
-	}
-		?>
+	include "main.html";
+	chance:
+	
+}
+else
+	{$_SESSION['status']="Please Enter Password";
+            $_SESSION['status_code']="error";
+	include "main.html";
+}
+}
+else
+{$_SESSION['status']="Please Enter Email";
+            $_SESSION['status_code']="error";
+ 
+	include "main.html";
+	
+} 
+?>
+
+		
 		<?php
 		include 'script_log.php';
 		?>
+
 		
