515ee9698180c9770c535eb16e71ce837a61288a
Rajat376 <89566014+Rajat376@users.noreply.github.com>
2021-10-11 20:05:17 +0530

    Update script_log.php
    made code short

--- script_log.php
+++ script_log.php
@@ -1,12 +1,6 @@
 
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.bundle.min.js"
-    integrity="sha384-U1DAWAznBHeqEIlVSCgzq+c9gqGAJn5c/t99JyeKa9xxaYpSvHU5awsuZVVFIhvj"
-    crossorigin="anonymous"></script>
-  <script src="https://cdn.lordicon.com/libs/mssddfmo/lord-icon-2.1.0.js"></script>
-  <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
-<?php
-if(isset($_SESSION['status']) && $_SESSION['status']!=''){
-    ?>
+<script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
+
     <script>
     swal({
   title: "<?php echo $_SESSION['status']; ?>",
@@ -14,14 +8,3 @@ if(isset($_SESSION['status']) && $_SESSION['status']!=''){
   icon: "<?php echo $_SESSION['status_code']; ?>",
 });
 </script>
-    <?php
-    unset($_SESSION['status']);
-}
-?>
-<!-- <script>
-    swal({
-  title: "Good job!",
-  text: "You clicked the button!",
-  icon: "success",
-});
-</script> -->
