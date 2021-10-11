9aafb0d7815136dc6142653e852987c98228ac3c
Rajat376 <89566014+Rajat376@users.noreply.github.com>
2021-10-11 20:03:55 +0530

    Update signup.php
    same stuff as login.php

--- signup.php
+++ signup.php
@@ -1,7 +1,6 @@
 <?php
-session_start();
-?>
-<?php
+
+ session_start();
 $link = mysqli_connect('localhost', 'root', '', 'jk');
 $your_name=$_POST['your_name'];
 $username=$_POST['username'];
@@ -12,26 +11,35 @@ die('Failed to connect to server');
 } 
 $query="INSERT INTO users (your_name,username,email,password) Values ('$your_name','$username','$email','$password')";
 $results = mysqli_query($link, $query); 
-
+if($your_name)
+{if($password)
+	{
 if($results == FALSE) 
 {
-// echo '<script type="text/javascript">
-//           window.onload = function () { alert("Usename taken or email already registered or all field not filled"); }
-// </script>';
-$_SESSION['status']="Please Fill the form Again";
-$_SESSION['status_code']="error";
-
-include 'main.html';
+$_SESSION['status']="Email or Username taken or not filled";
+            $_SESSION['status_code']="error";
+			include "main.html";
 }
 else {
-// 	echo'<script type="text/javascript">
-//           window.onload = function () { alert("Thank you for signing up"); }
-// </script>';
-$_SESSION['status']="Registred Successfully !!";
-$_SESSION['status_code']="success";
-include 'main.html';
+	$_SESSION['status']="You are signed up";
+            $_SESSION['status_code']="success";
+			include "main.html";
+
 } 
+	}
+
+else{
+	$_SESSION['status']="Please Enter Password";
+            $_SESSION['status_code']="error";
+			include "main.html";
+}
+}
+
+
+else
+{$_SESSION['status']="Please fill your name";
+            $_SESSION['status_code']="error";
+			include "main.html";
+}
+include "script_log.php";
 ?> 
-<?php
-include 'script_sign.php';
-?>
