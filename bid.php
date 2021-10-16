8679cde3422218c8c24c5ec5e593c019855d25d7
Rajat376 <89566014+Rajat376@users.noreply.github.com>
2021-10-16 11:49:08 +0530

    Update bid.php

--- bid.php
+++ bid.php
@@ -3,13 +3,14 @@ session_start();
 ?>
 <?php
 
-
+if(isset($_POST['bid']))
+{
 if(isset($_SESSION['IS_AUTHENTICATED']) && $_SESSION['IS_AUTHENTICATED'] == 1)
 {$link = mysqli_connect('localhost', 'root', '', 'jk');
 if(!$link){ 
 die('Failed to connect to server'); 
 } 
-if($_SESSION['HiredId'] == $_SESSION['USER_EMAIL'])
+if($_SESSION['HIRER_ID'] == $_SESSION['USER_EMAIL'])
 {$_SESSION['status']="Can't Bid on Your own Project";
             $_SESSION['status_code']="error";
 	include"main.html";
@@ -19,11 +20,12 @@ else
 {$BidData=$_POST['BidData'];
 $Date=$_POST['Date'];
 $Amount=$_POST['Amount'];
-$HirerId=$_SESSION['HIRER_ID'];
+
 $HirerName=$_SESSION['HirerName'];
+$HirerId=$_SESSION['HIRER_ID'];
 $BidderName=$_SESSION['USER_NAME'];
 $BidderId=$_SESSION['USER_EMAIL'];
-$ProjectId=$_SESSION['ProjectId'];
+$ProjectId=$_POST['ProjectId'];
 $query="INSERT INTO bidding(HirerId	,HirerName,	BidderName,	BidderId,	BidData,	Amount,	FinalDate,	ProjectId) VALUES ('$HirerId','$HirerName','$BidderName','$BidderId','$BidData','$Amount','$Date','$ProjectId')";
 $results = mysqli_query($link,$query); 
 
@@ -37,5 +39,8 @@ else
             $_SESSION['status_code']="error";
 	include"main.html";
 }
-	include"script_log.php";	
+	include"script_log.php";
+}
+else	
+	header('Location: ' . $_SERVER['HTTP_REFERER']);
 ?>
